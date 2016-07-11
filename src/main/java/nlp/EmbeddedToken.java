5cc7be89c5f9769c10f6ce9cf18e4e81cac3239b
eellpp <eellpp9@gmail.com>
2016-07-11 08:12:28 +0800

    initial

---
+++ src/main/java/nlp/EmbeddedToken.java
@@ -0,0 +1,21 @@
+package nlp;
+
+class EmbeddedToken {
+
+	  private String name;
+	  private String value;
+
+	  public String getName() {
+	    return name;
+	  }
+
+	  public String getValue() {
+	    return value;
+	  }
+
+	  public EmbeddedToken(String name, String value) {
+	    super();
+	    this.name = name;
+	    this.value = value;
+	  }
+}
